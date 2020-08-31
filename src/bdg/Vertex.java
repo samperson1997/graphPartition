c3655252b87a77772bc9a7b4d7e17bc4bf1d497a
sunzesong <2474809464@qq.com>
2020-08-31 13:15:26 +0800

    add BDG partition (in Java)

---
+++ src/bdg/Vertex.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.List;
+
+public class Vertex {
+
+  private String name;
+  private List<Vertex> neighbors;
+  private int color;
+
+  public Vertex(String name, List<Vertex> neighbors) {
+    this.name = name;
+    this.neighbors = neighbors;
+    this.color = -1;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Vertex> getNeighbors() {
+    return neighbors;
+  }
+
+  public void setNeighbors(List<Vertex> neighbors) {
+    this.neighbors = neighbors;
+  }
+
+  public int getColor() {
+    return color;
+  }
+
+  public void setColor(int color) {
+    this.color = color;
+  }
+}
